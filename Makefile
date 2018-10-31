fafec34d898f3c73973b881c502691e420c55604
Thomas Alisi <thomas.alisi.ex@pirelli.com>
2018-10-31 15:04:37 +0000

    fix lambda

--- Makefile
+++ Makefile
@@ -1,14 +1,22 @@
 CURRENT_DIR?=$(abspath .)
 INFRASTRUCTURE_DIR=infrastructure
 
+help:
+	@echo "make, list of available commands (order is important!)"
+	@echo "--------------------------------"
+	@echo "> lambda-readings:   builds the lambda for deployment"
+	@echo "> init:              initialise terraform modules"
+	@echo "> infrastructure:    builds with terraform"
+	@echo "> destroy:           destroys with terraform (handle with care!)"
+
+lambda-readings:
+	OUTPUT_DIR="${CURRENT_DIR}/infrastructure" make -C lambda/readings build
+
 init:
 	cd ${INFRASTRUCTURE_DIR} && terraform init
 
-infrastructure: init lambda-readings
+infrastructure:
 	cd ${INFRASTRUCTURE_DIR} && terraform apply
 
 destroy:
 	cd ${INFRASTRUCTURE_DIR} && terraform destroy
-
-lambda-readings:
-	OUTPUT_DIR="${CURRENT_DIR}/infrastructure" make -C lambda/readings build
--- lambda/readings/lambda.py
+++ lambda/readings/lambda.py
@@ -1,8 +1,38 @@
-def run(event, context):
-    return {
-        'message': 'hello',
-    }
+import os
+import json
 
+def run(event, context):
+    """main lambda handler, should route requests to specific functions depending on path
+    the implementation is beyond the scope of this demo, but in case it's needed, this
+    seems to be a good starting point: https://github.com/trustpilot/python-lambdarest
+
+    for details of contents received in the event dictionary when integrated with api gw
+    check https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+
+    for details of the context object, check https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    content-type of response is automatically set to application/json when lambda is called
+    via an api gw proxy
+
+    :param event: the result of an api gw integration with this lambda
+    :type event: dictionary
+    :param context: runtime information, check docs for content
+    :type context: aws lambdacontext object
+    :return: a processed response
+    :rtype: httpresponse
+    """
+
+    env_vars = ['DYNAMODB_TABLE']
+    body = {
+        "event": event,
+        "env": {v: os.getenv(v) for v in env_vars}
+    }
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {},
+        "body": json.dumps(body)
+    }
 
 if __name__ == '__main__':
     run()
