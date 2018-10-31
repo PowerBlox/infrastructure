9ac777a518268e7808dc2976f7e223b3f4368641
Thomas Alisi <thomas.alisi.ex@pirelli.com>
2018-10-31 15:15:39 +0000

    s3 buckets

---
+++ infrastructure/storage/main.tf
@@ -0,0 +1,28 @@
+variable namespace {}
+
+variable stage_env {}
+
+resource "aws_s3_bucket" "logging" {
+  bucket = "${var.namespace}-device-readings-${var.stage_env}-logs"
+  acl    = "log-delivery-write"
+
+  tags {
+    Name        = "Logs for device readings ${var.stage_env}"
+    Environment = "${var.stage_env}"
+  }
+}
+
+resource "aws_s3_bucket" "storage" {
+  bucket = "${var.namespace}-device-readings-${var.stage_env}"
+  acl    = "private"
+
+  logging {
+    target_bucket = "${aws_s3_bucket.logging.id}"
+    target_prefix = "log/"
+  }
+
+  tags {
+    Name        = "Storage for device readings ${var.stage_env}"
+    Environment = "${var.stage_env}"
+  }
+}
