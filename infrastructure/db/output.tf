c230e41a735ef65578eb8aa3538c31846dc0c884
Thomas Alisi <thomas.alisi.ex@pirelli.com>
2018-10-31 15:15:23 +0000

    mysql server

--- infrastructure/db/output.tf
+++ infrastructure/db/output.tf
@@ -1,4 +1,8 @@
 
-output "dynamodb_table_readings" {
+output "dynamodb_table_readings_name" {
   value = "${aws_dynamodb_table.readings.name}"
+}
+
+output "dynamodb_table_readings_arn" {
+  value = "${aws_dynamodb_table.readings.arn}"
 }
