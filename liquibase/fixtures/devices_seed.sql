f3735fbb53d425d8c2990bf2648aed5a00453d72
Thomas Alisi <thomas.alisi.ex@pirelli.com>
2018-11-15 11:16:27 +0000

    update migrations with dani's table and import seed

---
+++ liquibase/fixtures/devices_seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO `devices` (`id`, `name`, `hardware_mac_id`, `hardware_rev`, `software_ref`, `base_station_id`, `created_at`, `modified_at`, `created_by`, `moified_by`, `HARDWAREID`) VALUES
+(1, 'D0ADE3', '00:06:66:D0:AD:E3', '', '', 'b8:27:eb:37:93:e0', '2018-11-12 15:51:03', '0000-00-00 00:00:00', 0, 0, 0);
